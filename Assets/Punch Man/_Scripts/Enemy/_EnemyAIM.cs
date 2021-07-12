ecc164ba9f38b19b64b925cc54c97fe10a453dee
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2021-07-12 11:55:07 +0530

    Update

---
+++ Assets/Punch Man/_Scripts/Enemy/_EnemyAIM.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class _EnemyAIM : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    private GameObject Player;
+    private Animator EnemyAI;
+    private bool isDead = false;
+    private float turnSmoothVelocity;
+
+    public float maxHealth = 75;
+    public float rotationSmooth = 10;
+
+
+    [HideInInspector]public float CurrentHealth;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        Player = GameObject.Find("Hand Holder");
+        EnemyAI = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        this.agent.SetDestination(Player.transform.position);
+        EnemyAI.Play("Running");
+        if (agent.velocity.magnitude > 0)
+        {
+            float targetAngle = Mathf.Atan2(agent.velocity.x, agent.velocity.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, rotationSmooth);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+        }
+    }
+}
---
+++ Assets/Punch Man/_Scripts/Player/_PlayerHandController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _PlayerHandController : MonoBehaviour
+{
+    private Animator handAnimation;
+    [SerializeField] private bool HandChecker;
+    [SerializeField] private bool longPress = false;
+    [SerializeField] private float downPressTime;
+    [SerializeField] private float longPressTime;
+    private float force = 0;
+
+    public ParticleSystem superEffect;
+    void Start()
+    {
+        handAnimation = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        handA();
+    }
+    void handA()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            downPressTime += Time.deltaTime;
+            if (downPressTime > longPressTime)
+            {
+                longPress = true;
+                handAnimation.Play("Long");
+                superEffect.Play();
+            }
+            else
+            {
+                longPress = false;
+            }
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (!longPress)
+            {
+                force = 100;
+                downPressTime = 0;
+                if (handAnimation.GetCurrentAnimatorStateInfo(0).IsName("Left") && handAnimation.GetCurrentAnimatorStateInfo(0).IsName("Right"))
+                    return;
+
+                if (!HandChecker)
+                {
+                    HandChecker = true;
+                }
+                else if (HandChecker)
+                {
+                    HandChecker = false;
+                }
+
+                if (HandChecker)
+                    handAnimation.Play("Right");
+
+                if (!HandChecker)
+                    handAnimation.Play("Left");
+            }
+            if (longPress)
+            {
+                force = 10000;
+                downPressTime = 0;
+                handAnimation.Play("PowerHit");
+                superEffect.Stop();
+            }
+        }
+
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.collider.name+" : Hit");
+        other.transform.GetComponent<Rigidbody>().AddForce(-other.transform.forward * force);
+    }
+
+}
